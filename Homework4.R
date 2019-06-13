543469a5704681f1907837783b6d10deae405295
Vanessa Rivera-Quinones <43471944+vriveraq@users.noreply.github.com>
2019-06-13 10:53:01 -0500

    Update and rename HW4_490ID_41.R to Homework4.R

--- Homework4.R
+++ Homework4.R
@@ -1,12 +1,5 @@
-# HW 4 Due Monday Oct 2, 2017. 
-# Upload R file to Moodle with name: HW4_490ID_41.R
-
-# Your classID: 41
-
-# Notice we are using the new system with your unique class ID. You should have received an email with
-# your unique class ID. Please make sure that ID is the only information on your hw that identifies you. 
-
-# Do not remove any of the comments. These are marked by #
+# GOAL: Understand sampling and linear regression concepts
+# DATA: Cats data set (Part 2) and  prestige data set from cars library (Part 3).
 
 ################## Part 1: Linear Regression Concepts #######################
 
@@ -16,14 +9,12 @@
 ##    y = b0 + b1*x
 ## This homework will not discuss other models.
 
-## 1. (1 pt)
-## What is the interpretation of the coefficient B1? 
-## (What meaning does it represent?)
+## 1.  What is the interpretation of the coefficient B1? (What meaning does it represent?)
 
 ## Your answer here
 ### B1 gives us the increase in y for every unit increase in x.
 
-## 2. (1 pt)
+## 2. 
 ## Outliers are problems for many statistical methods, but are particularly problematic
 ## for linear regression. Why is that? It may help to define what outlier means in this case.
 ## (Hint: Think of how residuals are calculated)
@@ -34,16 +25,11 @@
 ### residuals. Thus, outliers have higher errors and can "skew" the optimal line.  
 
 
-## 3. (1 pt)
-## How could you deal with outliers in order to improve the accuracy of your model?
+## 3.  How could you deal with outliers in order to improve the accuracy of your model?
 
 ## Your answer here
-
 ### It depends. If the outlier seems like an error (in data collection or recording) then one could remove it from the data.
 ### If that is not the case, one must be careful since it might be the case we need a better model.
-
-
-
 
 ############# Part 2: Sampling and Point Estimation #####################
 
@@ -56,11 +42,9 @@ install.packages("MASS")
 library(MASS)
 data(cats)
 
-## 4. (2 pts)
-## Subset the data frame to ONLY include female cats.
+## 4. Subset the data frame to ONLY include female cats.
 
 ## Your answer here
-
 female.cats =cats[cats$Sex == "F",]
 head(female.cats)
 
@@ -76,13 +60,14 @@ head(female.cats)
 ## in Bwt2.
 
 set.seed(676)
+
 ## Your answer here
 dim(female.cats) 
 fcats.idx = sample(c(1,2),47, replace = TRUE)
 Bwt1 = female.cats$Bwt[fcats.idx==1]
 Bwt2 = female.cats$Bwt[fcats.idx==2]
 
-## 5. (3 pts)
+## 5. 
 ## Calculate the mean and the standard deviation for each of the two
 ## vectors, Bwt1 and Bwt2. Use this information to create a 95% 
 ## confidence interval for your sample means (you can use the following formula 
@@ -101,7 +86,7 @@ CI.Bwt2 = print(c(mean.Bwt2 -2*sd.Bwt2, mean.Bwt2+2*sd.Bwt2))
 
 
 
-## 6. (4 pts)
+## 6.
 ## Draw 1000 observations from a standard normal distribution. Calculate the sample mean.
 ## Repeat this 500 times, storing each sample mean in a vector called mean_dist.
 ## Plot a histogram of mean_dist to display the distribution of your sample mean.
@@ -142,8 +127,7 @@ HW.Bootstrap(rexp,1000,500)
 
 ################### Part 3: More Linear Regression ######################
 
-## This problem will use the Prestige dataset.
-## Load the data by running code below
+## This problem will use the Prestige dataset. Load the data by running code below
 install.packages("car")
 library(car)
 data(Prestige)
@@ -159,18 +143,17 @@ data(Prestige)
 ## We will attempt to reject this hypothesis by using a linear regression
 
 
-## 8. (2 pt)
+## 8. 
 ## Fit a linear regression using of Prestige data using education to predict
 ## income, using lm(). Examine the model diagnostics using plot(). Would you 
 ## consider this a good model or not? Explain.
 
 ## Your answer here
-
 model = lm(Prestige$income~Prestige$education)
 plot(Prestige$education,Prestige$income,xlab = "Education",ylab = "Income")
 abline(model)
 
-## 9. (2 pts)
+## 9. 
 ## Using the information from summary() on your model (the output from the lm() command), create a 
 ## 95% confidence interval for the coefficient of education variable 
 
@@ -180,7 +163,7 @@ lower.bound = model$coefficients[2] - 2*summary(model)$coefficients[2,2]
 upper.bound = model$coefficients[2] + 2*summary(model)$coefficients[2,2]
 CI = print(c(lower.bound,upper.bound))
 
-## 10. (2 pts)
+## 10. 
 ## Based on the result from question 9, would you reject the null hypothesis or not?
 ## (Assume a significance level of 0.05). Explain.
 
@@ -188,9 +171,7 @@ CI = print(c(lower.bound,upper.bound))
 ### I would reject the null hypothesis since the interval is far and does not contain zero. 
 ### Suggesting there is a relationship between education and income.
 
-
-
-## 11. (1 pt)
+## 11. 
 ## Assuming that the null hypothesis is true. 
 ## Based on your decision in the previous question, would you be committing a decision error? 
 ## If so, which type of error?
@@ -198,20 +179,10 @@ CI = print(c(lower.bound,upper.bound))
 ## Your answer here
 ### If we assume the null hypothesis is true, and we reject it we would have a Type I error (False Positive).
 
-
-## 12. (1 pt)
-## Discuss what your regression results mean in the context of the data.
-## (Hint: Think back to Question 1)
+## 12. 
+## Discuss what your regression results mean in the context of the data. (Hint: Think back to Question 1)
 
 ## Your answer here
 ### From our model, we obtain that the intercept is  -2853.6  while beta1 is 898.8. 
 ### This means that for every year of education we should observe and increase in income
 ### equivalent to 898.8.
-
-
-
-
-
-
-
-
